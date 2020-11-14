8d91f05729b2cbbff2092a91d151303c05e6fc3e
andreimorun01 <andreimorun01@yahoo.com>
2020-11-14 19:10:18 +0200

    clasa noua Amplificator

---
+++ Amplificator.cpp
@@ -0,0 +1,9 @@
+//
+// Created by andrei on 14.11.2020.
+//
+#include "Amplificator.h"
+
+Amplificator::Amplificator(std::string Nume_Amplificator)
+{
+    this->Nume_Amplificator=Nume_Amplificator;
+}
---
+++ Amplificator.h
@@ -0,0 +1,22 @@
+//
+// Created by andrei on 14.11.2020.
+//
+#include <iostream>
+
+#ifndef MAGAZIN_CHITARE_AMPLIFICATOR_H
+#define MAGAZIN_CHITARE_AMPLIFICATOR_H
+
+#endif //MAGAZIN_CHITARE_AMPLIFICATOR_H
+
+class Amplificator
+{
+private:
+    std::string Nume_Amplificator;
+public:
+    Amplificator(std::string Nume_Amplificator);
+    ~Amplificator()
+    {
+
+    }
+
+};
